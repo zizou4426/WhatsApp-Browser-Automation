8092b415ed6e1feaaed6379ff02ed3fa97c268f5
Chaitanya Vankadaru <chaitanya.vankadaru@gmail.com>
2019-11-07 11:18:03 +0530

    Updated X_Path
    Updated X_Path accordingly

--- Code/whatsapp.py
+++ Code/whatsapp.py
@@ -1,6 +1,3 @@
-# Importing libraries
-# If you don't have selenium, install it using "pip3 install selenium" in your command prompt or powershell
-
 from selenium import webdriver 
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC 
@@ -12,7 +9,7 @@ import time
 options = webdriver.ChromeOptions()
 
 # Replace --user-data-dir="C:/Users/Chaitanya V/AppData/Local/Google/Chrome/User Data" with your path
-options.add_argument("--user-data-dir=C:/Users/Chaitanya V/AppData/Local/Google/Chrome/User Data")
+options.add_argument("--user-data-dir=C:/Users/chaitanya.vankadaru/AppData/Local/Google/Chrome/User Data")
 
 # Replace below path with the executable path to chromedriver in your computer ( You have to download version specific chromedriver) 
 driver = webdriver.Chrome(executable_path='C:/chromedriver_win32/chromedriver.exe', chrome_options=options)
@@ -21,14 +18,14 @@ driver.get("https://web.whatsapp.com/")
 wait = WebDriverWait(driver, 600) 
 
 # Replace 'Friend's Name' with the name of your friend or the name of a group 
-target = '"Zuckerberg"'
+target = 'Zukcerberg"'
 #target = '"Friend Name/Group Title"'
 
 # Replace the below string with your own message 
-string = "Data is money, right?"
+string = "Delhi is the new Chernobyl,right?"
 # string = "Your Message"
 
-x_arg = '//*[@id="pane-side"]/div[1]/div/div/div[4]/div/div/div[2]/div[1]/div[1]/span/span[contains(@title,' + target + ')]'
+x_arg = '//*[@id="pane-side"]/div[1]/div/div/div[*]/div/div/div[*]/div[*]/div[*]/span/span[contains(@title,' + target + ')]'
 group_title = wait.until(EC.presence_of_element_located(( 
 	By.XPATH, x_arg))) 
 group_title.click() 
@@ -43,4 +40,4 @@ for i in range(5): # Increase the range to send continuous messages range(n) imp
   
 # This code is solely to be used for academic purposesonly. I'm not responsible if you get banned due to this.
 
-# Note: This code won't work if your entered friend name/group title is in archieved chats.
+# Note: This code won't work if your input friend's name/group title is in archieved chats/or if they are not on homescreen of WhatsApp.
