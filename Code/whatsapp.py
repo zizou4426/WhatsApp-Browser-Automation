5e01c6739a12da9f12d52b908bdb1476de375a26
Chaitanya Vankadaru <chaitanya.vankadaru@gmail.com>
2020-02-01 13:29:03 +0530

    Updated X_Path and fixed few bugs rel. Selenium

--- Code/whatsapp.py
+++ Code/whatsapp.py
@@ -3,7 +3,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 from selenium.webdriver.common.keys import Keys 
 from selenium.webdriver.common.by import By 
-import time 
+import time, os 
 
 
 options = webdriver.ChromeOptions()
@@ -17,27 +17,32 @@ driver = webdriver.Chrome(executable_path='C:/chromedriver_win32/chromedriver.ex
 driver.get("https://web.whatsapp.com/") 
 wait = WebDriverWait(driver, 600) 
 
-# Replace 'Friend's Name' with the name of your friend or the name of a group 
-target = 'Zukcerberg"'
-#target = '"Friend Name/Group Title"'
+# Replace 'Recipient Name' with the name of your friend or the name of a group 
+x_arg= '//*[contains(@title,"Zuckerberg")]'
+# x_arg= '//*[contains(@title,"Recipient Name")]'
 
 # Replace the below string with your own message 
-string = "Delhi is the new Chernobyl,right?"
+string = "So, how's everything going on?"
 # string = "Your Message"
 
-x_arg = '//*[@id="pane-side"]/div[1]/div/div/div[*]/div/div/div[*]/div[*]/div[*]/span/span[contains(@title,' + target + ')]'
 group_title = wait.until(EC.presence_of_element_located(( 
 	By.XPATH, x_arg))) 
+
 group_title.click() 
 # inp_xpath = '//div[@class="input"][@dir="auto"][@data-tab="1"]'
 inp_xpath = '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]'
 input_box = wait.until(EC.presence_of_element_located(( 
 	By.XPATH, inp_xpath))) 
-for i in range(5): # Increase the range to send continuous messages range(n) implies n messages
+for i in range(1): # Increase the range to send continuous messages range(n) => 'n' messages
 	input_box.send_keys(string + Keys.ENTER) 
-	time.sleep(1) 
-  
-  
-# This code is solely to be used for academic purposesonly. I'm not responsible if you get banned due to this.
+	time.sleep(1) # Increase the sleep timer if you want the time interval between messages.
 
-# Note: This code won't work if your input friend's name/group title is in archieved chats/or if they are not on homescreen of WhatsApp.
+delete_paths = ['../selenium/chrome_profile/Local State',
+                '../selenium/chrome_profile/Default/Preferences']
+for delete_path in delete_paths:
+    if os.path.exists(delete_path):
+        os.remove(delete_path)
+  
+# This code is solely to be used for academic purposes only. I'm not responsible if you get into trouble or get banned due to this.
+
+# Note: This code won't work if your input friend's name/group title is in archieved chats/or if they aren't on homescreen of WhatsApp.
